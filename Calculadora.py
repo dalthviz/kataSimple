e608d7d5b8a2895be85b08235069be6b825241bb
santi <js.ardila1068@uniandes.edu.co>
2017-03-21 21:11:49 -0500

    Paso 5 - se hace el refactoring del metodo de suma para que pase la prueba cuando se ingresa una cadena con varios numeros con distintos separadores (Verde)

--- Calculadora.py
+++ Calculadora.py
@@ -2,11 +2,13 @@ class Calculadora:
     def sumar(self, cadena):
         if cadena == "":
             return 0
-        elif  "," in cadena:
+        elif  "," in cadena or "&" in cadena or ":" in cadena:
+            cadena=cadena.replace("&",",")
+            cadena=cadena.replace(":",",")
             numeros = cadena.split(",")
             suma = 0
             for num in numeros:
-                suma+=int(num)
+                suma=suma+int(num)
             return suma
         else:
             return int(cadena)
